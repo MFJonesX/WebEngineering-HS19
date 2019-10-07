c1e6b9ced6edab7bca3f229a35f873961872fdca
Adhurim Gervalla <adi@hausformat.com>
2019-10-07 17:26:27 +0200

    Added imperativ/deklarativ validation to controller

--- grails-app/controllers/mvc/CalculatorController.groovy
+++ grails-app/controllers/mvc/CalculatorController.groovy
@@ -1,18 +1,41 @@
 package mvc
 
+import grails.validation.Validateable
+
 class CalculatorController {
 
     def calc(CalculatorModel calculatorModel) {
         double result = (calculatorModel.en + calculatorModel.exam) / 2
-        calculatorModel.result = result
+        // Inverstigate 'calculatorModel.errors' to find out which field is concerned.
+        // Refer to 'feature/validation' branch
+
+        /* Imperative Validierung - START */
+        if (1 > calculatorModel.en || calculatorModel.en > 6){
+            calculatorModel.result = "en input is wrong"
+        } else if (1 > calculatorModel.exam || calculatorModel.exam > 6) {
+            calculatorModel.result = "exam input is wrong"
+        } else {
+            calculatorModel.result = result
+        }
+        /* Imperative Validierung - END */
         render view: "CalculatorOutput", model: [model: calculatorModel]
     }
 }
 
-class CalculatorModel {
+// Für eine Deklarative Validierung, muss das Validateable Interface implementiert werden
+class CalculatorModel implements Validateable {
 
     double en
     double exam
-    double result
+    String result
+
+    /* Deklarative Validierung - START */
+    /* Wurde jetzt hier noch nicht angewendet */
+    static constraints = {
+        en min:1d, max: 6d, scale: 1
+        exam min:1d, max: 6d, scale: 1
+        result nullable: true
+    }
+    /* Deklarative Validierung - END */
 
 }
